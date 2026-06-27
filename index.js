842b0c8614dc4b7d6c9c4b27fe76945cdcd3f51b
claudio <claudio.a.fernandeztoledo@gmail.com>
2026-06-27 18:30:18 -0400

    modifique html y javascript

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-precio = 400000
+const precioBase = 400000;
+let cantidad = 0;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+// CLAVE: Esto obliga al navegador a esperar que cargue todo el HTML antes de inyectar el precio inicial
+window.onload = function() {
+    document.querySelector('.precio-inicial').innerHTML = precioBase;
+    document.querySelector('.valor-total').innerHTML = 0;
+};
+
+// Estas funciones se ejecutan directo al presionar los atributos onclick del HTML
+function aumentarCantidad() {
+    cantidad++;
+    document.querySelector('.cantidad').innerHTML = cantidad;
+    document.querySelector('.valor-total').innerHTML = cantidad * precioBase;
+}
+
+function disminuirCantidad() {
+    if (cantidad > 0) {
+        cantidad--;
+        document.querySelector('.cantidad').innerHTML = cantidad;
+        document.querySelector('.valor-total').innerHTML = cantidad * precioBase;
+    }
+}
